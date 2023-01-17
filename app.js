439f88dfb1120838e15eea031290132312687584
yami <yamila.acunia@gmail.com>
2023-01-17 17:29:00 -0300

    se agrega proyecto

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+//app.listen(3000, ()=>{
+  //  console.log('Servidor funcionando');
+//});
+
+const port = process.env.PORT || 001;
+app.listen(port,()=> console.log ('servidor corriendo en puerto $(port)'));
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
