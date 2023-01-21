ab36e5232eaf7d649876523c0087c59b7a166c32
yami <yamila.acunia@gmail.com>
2023-01-21 15:17:16 -0300

    error port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 //implemento rutas
 
